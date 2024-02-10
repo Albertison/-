0e4bb6ced799f883b888790ae03fdf0e508afd31
albert <xazivalieva2@gmail.com>
2024-02-10 09:52:11 +0300

    Изображение Марса

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Миссия Колонизация Марса"
+
+
+@app.route("/index")
+def index():
+    return "И на Марсе будут яблони цвести!"
+
+
+@app.route("/promotion")
+def erion():
+    aer = ["Человечество вырастает из детства.", "Человечеству мала одна планета.",
+           "Мы сделаем обитаемыми безжизненные пока планеты.", "И начнем с Марса!", "Присоединяйся!"]
+    return '<br>'.join(aer)
+
+
+@app.route("/image_mars")
+def marsian():
+    with open("image_mars.html", 'r', encoding='utf-8') as html_str:
+        return html_str.read()
+
+
+if __name__ == "__main__":
+    app.run(port=8080, host="127.0.0.1")
